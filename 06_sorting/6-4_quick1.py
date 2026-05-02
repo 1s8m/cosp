3aaf6a342e0771c64002286d45970575a88dff8a
Brian S. <1s8m@proton.me>
2026-05-02 16:49:41 +0000

    20260503

--- 06_sorting/6-4_quick1.py
+++ 06_sorting/6-4_quick1.py
@@ -1,4 +1,5 @@
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+array = [7, 5, 9, 0, 3, 1, 6, 2, 5, 4, 8]
 
 def quick_sorting1(array, start, end):
     if start >= end: # 원소가 1개인 경우에는 종료!
@@ -8,10 +9,10 @@ def quick_sorting1(array, start, end):
     left = start + 1 # 시작 인덱스는 두번째 인덱스
     right = end # 끝 인덱스는 마지막 인덱스
 
-    while left <= right: # left와 right가 완전 교차하지 않고 일치하는 케이스에 대해서도 아래 프로세스 진행시켜야 함.
-        while left <= end and array[left] <= array[pivot]: 
+    while left <= right: # left와 right가 완전 교차하지 않고 일치하는 케이스에 대해서도 아래 프로세스 진행시켜야 함. # 동일한 원소가 존재할 수 있으므로 등호 필요
+        while left <= end and array[left] <= array[pivot]: # left vs. pivot 동일한 원소가 존재할 수 있으므로 등호가 필요
             left += 1 # array[left] > array[pivot] 찾을 때까지 왼쪽의 인덱스는 1씩 증가시킴!
-        while right > start and array[right] >= array[pivot]:
+        while right > start and array[right] >= array[pivot]: # right vs. pivot 동일한 원소가 존재할 수 있으므로 등호가 필요
             right -= 1 # array[right] < array[pivot] 찾을 때까지 왼쪽의 인덱스는 1씩 증가시킴!
         if left > right: # left, right 가 엇갈림
             array[right], array[pivot] = array[pivot], array[right]
